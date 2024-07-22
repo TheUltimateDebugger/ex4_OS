f2d2cebb7f9724b90c4ab75648de88e1d33d9ab1
Tomer <tomer.zilberman.44@gmail.com>
2024-07-22 11:19:05 +0300

    VMinitialize completed not tested

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,8 +1,12 @@
 #include "VirtualMemory.h"
-
+#include "PhysicalMemory.h"
 
 void VMinitialize(){
-
+  // initialize everything to zero. the tree is yet to be created
+  for (uint64_t i = 0; i < pow(2, PHYSICAL_ADDRESS_WIDTH); ++i)
+  {
+    PMwrite (i, 0);
+  }
 }
 
 
