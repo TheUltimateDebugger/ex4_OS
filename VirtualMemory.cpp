fd7ec6b92f7f835868c52654dea57565d8ee0585
Tomer <tomer.zilberman.44@gmail.com>
2024-07-22 13:31:13 +0300

    find_unused_frame completed fixed

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
@@ -5,40 +6,59 @@
 #define SMALL_OFFSET (VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH) % OFFSET_WIDTH
 
 uint64_t find_unused_frame_recursive(uint64_t current_page, uint_least64_t
-*max_used){
+*max_used, uint64_t depth){
   word_t value;
-  uint64_t addr, result = -1;
+  uint64_t addr, result = -2;
+
+  if (*max_used < current_page) //updates max used tree
+    *max_used = current_page;
+
+  if (depth == TABLES_DEPTH) //page is not part of the tree
+    return -1;
+
   for (int i = 0; i < pow(2, OFFSET_WIDTH); ++i)
   {
     PMread (current_page, &value);
     addr = value;
-    result = find_unused_frame_recursive (addr, max_used);
+    if (addr != 0)
+    {
+      result = find_unused_frame_recursive (addr, max_used, depth + 1);
+
+      if (result != -1 && result != -2) //we have a good result
+        return result;
+    }
   }
-  if (result == -1)
-    if (max_used < NUM_FRAMES + 1)
-      return max_used + 1;
-    else
-      return -1;
-  return -1;
+  //if result has not changed then current_page has no sons
+  if (result == -2)
+    result = current_page;
+
+  return result;
 }
 
 uint64_t find_unused_frame(){
-  uint64_t max_used = 0;
+  uint64_t max_used = 1;
   word_t value;
-  uint64_t addr, result = -1;
+  uint64_t addr, result = -2;
   for (int i = 0; i < pow(2, SMALL_OFFSET); ++i)
   {
     PMread (0, &value);
     addr = value;
-    result = find_unused_frame_recursive (addr, &max_used);
+    if (addr != 0)
+    {
+      result = find_unused_frame_recursive (addr, &max_used, 1);
 
+      if (result != -1 && result != -2) //we have a good result
+        return result;
+    }
   }
+  //if result has not changed then 0 has no sons
+  if (result == -2)
+    return 0; //the current frame is 0
   if (result == -1)
     if (max_used < NUM_FRAMES + 1)
       return max_used + 1;
     else
       return -1;
-
 }
 
 
